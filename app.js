2b3ae6e6f43ab7f9f5eb32925a1f747f884b5936
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2026-05-03 17:00:05 +0300

    setup express-handlebars; add placeholder content to home.handlebars

---
+++ app.js
@@ -0,0 +1,9 @@
+// const { engine } = require('express-handlebars');
+
+// // Handlebars setup
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+// app.set('views', './src/views');
+
+// Hilmalle: Tällä funktiolla renderöi home.handlebars-tiedoston
+// res.render("home");
