78839c95da10509dfba4c234e9a7c8521fb039ed
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2026-05-04 19:35:45 +0300

    setup express

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const express = require('express');
 const { engine } = require('express-handlebars');
 require('dotenv').config();
 const authentication = require('./src/middleware/authentication.js');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
 
 // Hilmalle: Poista kommentit näistä, kun express palvelin on luotu (muuttujan nimeksi app)
 // // Handlebars setup
