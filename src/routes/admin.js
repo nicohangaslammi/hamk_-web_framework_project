98edf84c177e6610a1c13e0ea981672320811326
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2026-05-05 20:09:03 +0300

    add authentication and findOne to create draw

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const authentication = require('../middleware/authentication');
 const DrawRound = require('../models/DrawRound');
 
-router.post('/rounds', async (req,res) => {
+router.post('/rounds', authentication, async (req,res) => {
     try {
+        const activeRound = await DrawRound.findOne({ status: 'auki' });
+        if (activeRound) {
+            return res.send("Draw is already running!");
+        }
         const newRound = new DrawRound({
             status: 'auki',
             draw_time: new Date()
         });
-
         await newRound.save();
         res.send("Draw created!");
         }
