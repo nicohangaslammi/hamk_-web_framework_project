d65d4fdf567d026a4ad7873d87b96fbe0d034e9b
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2026-05-05 19:36:12 +0300

    added create draw no conditions

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,4 +4,21 @@ const router = express.Router();
 const authentication = require('../middleware/authentication');
 const DrawRound = require('../models/DrawRound');
 
+router.post('/rounds', async (req,res) => {
+    try {
+        const newRound = new DrawRound({
+            status: 'auki',
+            draw_time: new Date()
+        });
+
+        await newRound.save();
+        res.send("Draw created!");
+        }
+    catch (error) {
+        res.status(500).json({
+            msg: 'error'
+        });
+    }
+});
+
 module.exports = router;
