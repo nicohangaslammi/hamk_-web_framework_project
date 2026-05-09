a1af91e7ceae601f564b286ed9db155ba1df8d52
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2026-05-09 18:49:18 +0300

    add verifySocket function

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,8 +1,14 @@
 // WebSocket functions
 
+// Map object for storing WebSocket connections paired with randomly generated tokens
+const connections = new Map();
+
 // Verify WebSocket by checking if connections map contains given key-value pair
 const verifySocket = (socketId, token) => {
-    return true;
+    // Return false if either of given parameters is undefined
+    if (!socketId || !token) return false;
+
+    return (connections.get(socketId) === token);
 }
 
 module.exports = { verifySocket };
