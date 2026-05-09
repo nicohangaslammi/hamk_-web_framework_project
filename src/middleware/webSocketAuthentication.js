7dfe738088d41d67e8da6d30fe0c07b957d79e40
Nico Hangaslammi <nico.hangaslammi@gmail.com>
2026-05-09 18:30:12 +0300

    add placeholder src/middleware/webSocketAuthentication.js and src/services/socket.js functionality

---
+++ src/middleware/webSocketAuthentication.js
@@ -0,0 +1,6 @@
+const { verifySocket } = require('../src/services/socket.js');
+
+// WebSocket authentication middleware
+const webSocketAuthentication = (req, res, next) => {};
+
+module.exports = webSocketAuthentication;
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,1 +1,8 @@
 // WebSocket functions
+
+// Verify WebSocket by checking if connections map contains given key-value pair
+const verifySocket = (socketId, token) => {
+    return true;
+}
+
+module.exports = { verifySocket };
